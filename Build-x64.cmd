f199a1597e0dddc722af77621e812635912f5585
Miguel Nischor <miguelnischor@gmail.com>
2019-09-17 09:59:57 -0400

    Log implementation for build scripts.

--- Build-x64.cmd
+++ Build-x64.cmd
@@ -9,15 +9,16 @@ mkdir Build\Windows\x64\Debug\Assembly
 mkdir Build\Windows\x64\Debug\Log
 
 REM # [2] Source file compilation
-cl /c /O2 /Ot /Oi /favor:blend /std:c++17 /FaBuild\Windows\x64\Release\Assembly\WarlockEngine.asm /FmBuild\Windows\x64\Release\WarlockEngine.map /FoBuild\Windows\x64\Release\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp
-cl /c /Od /Ot /Oi /std:c++17 /FaBuild\Windows\x64\Debug\Assembly\WarlockEngine.asm /FdBuild\Windows\x64\Debug\WarlockEngine.pdb /FmBuild\Windows\x64\Debug\WarlockEngine.map /FoBuild\Windows\x64\Debug\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp
+cl /c /O2 /Ot /Oi /favor:blend /std:c++17 /FaBuild\Windows\x64\Release\Assembly\WarlockEngine.asm /FmBuild\Windows\x64\Release\WarlockEngine.map /FoBuild\Windows\x64\Release\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp  > Build\Windows\x64\Release\Log\Compiler.log
+cl /c /Od /Ot /Oi /std:c++17 /FaBuild\Windows\x64\Debug\Assembly\WarlockEngine.asm /FdBuild\Windows\x64\Debug\WarlockEngine.pdb /FmBuild\Windows\x64\Debug\WarlockEngine.map /FoBuild\Windows\x64\Debug\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp > Build\Windows\x64\Debug\Log\Compiler.log
 
 REM # [3] Resource file compilation
-rc /nologo /r /v /c 65001 /d WARLOCK_BUILD Source\WarlockEngine.rc
+rc /nologo /r /v /c 65001 /d WARLOCK_BUILD Source\WarlockEngine.rc > Build\Windows\x86\Release\Log\Resource.log
+copy Build\Windows\x64\Release\Log\Resource.log Build\Windows\x64\Debug\Log\Resource.log
 move Source\WarlockEngine.res Build\Windows\x64\Release\Object
 copy Build\Windows\x64\Release\Object\WarlockEngine.res Build\Windows\x64\Debug\Object
 del Build\Windows\x64\Release\WarlockEngine.res
 
 REM # [4] Dynamic link library creation
-link /nologo /DLL /SUBSYSTEM:WINDOWS Build\Windows\x64\Release\Object\WarlockEngine.res Build\Windows\x64\Release\Object\WarlockEngine.obj /OUT:Build\Windows\x64\Release\WarlockEngine.dll
-link /nologo /DEBUG:FULL /DLL /SUBSYSTEM:WINDOWS Build\Windows\x64\Debug\Object\WarlockEngine.res Build\Windows\x64\Debug\Object\WarlockEngine.obj /OUT:Build\Windows\x64\Debug\WarlockEngine.dll
+link /nologo /DLL /SUBSYSTEM:WINDOWS /VERBOSE Build\Windows\x64\Release\Object\WarlockEngine.res Build\Windows\x64\Release\Object\WarlockEngine.obj /OUT:Build\Windows\x64\Release\WarlockEngine.dll > Build\Windows\x64\Release\Log\Linker.log
+link /nologo /DEBUG:FULL /DLL /SUBSYSTEM:WINDOWS /VERBOSE Build\Windows\x64\Debug\Object\WarlockEngine.res Build\Windows\x64\Debug\Object\WarlockEngine.obj /OUT:Build\Windows\x64\Debug\WarlockEngine.dll > Build\Windows\x64\Debug\Log\Linker.log
--- Build-x86.cmd
+++ Build-x86.cmd
@@ -9,15 +9,16 @@ mkdir Build\Windows\x86\Debug\Assembly
 mkdir Build\Windows\x86\Debug\Log
 
 REM # [2] Source file compilation
-cl /c /O2 /Ot /Oi /favor:blend /std:c++17 /FaBuild\Windows\x86\Release\Assembly\WarlockEngine.asm /FmBuild\Windows\x86\Release\WarlockEngine.map /FoBuild\Windows\x86\Release\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp
-cl /c /Od /Ot /Oi /std:c++17 /FaBuild\Windows\x86\Debug\Assembly\WarlockEngine.asm /FdBuild\Windows\x86\Debug\WarlockEngine.pdb /FmBuild\Windows\x86\Debug\WarlockEngine.map /FoBuild\Windows\x86\Debug\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp
+cl /c /O2 /Ot /Oi /favor:blend /std:c++17 /FaBuild\Windows\x86\Release\Assembly\WarlockEngine.asm /FmBuild\Windows\x86\Release\WarlockEngine.map /FoBuild\Windows\x86\Release\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp > Build\Windows\x86\Release\Log\Compiler.log
+cl /c /Od /Ot /Oi /std:c++17 /FaBuild\Windows\x86\Debug\Assembly\WarlockEngine.asm /FdBuild\Windows\x86\Debug\WarlockEngine.pdb /FmBuild\Windows\x86\Debug\WarlockEngine.map /FoBuild\Windows\x86\Debug\Object\WarlockEngine.obj /nologo /MP2 /showIncludes /TP /utf-8 Source/WarlockEngine.cpp > Build\Windows\x86\Debug\Log\Compiler.log
 
 REM # [3] Resource file compilation
-rc /nologo /r /v /c 65001 /d WARLOCK_BUILD Source\WarlockEngine.rc
+rc /nologo /r /v /c 65001 /d WARLOCK_BUILD Source\WarlockEngine.rc > Build\Windows\x86\Release\Log\Resource.log
+copy Build\Windows\x86\Release\Log\Resource.log Build\Windows\x86\Debug\Log\Resource.log
 move Source\WarlockEngine.res Build\Windows\x86\Release\Object
 copy Build\Windows\x86\Release\Object\WarlockEngine.res Build\Windows\x86\Debug\Object
 del Build\Windows\x86\Release\WarlockEngine.res
 
 REM # [4] Dynamic link library creation
-link /nologo /DLL /SUBSYSTEM:WINDOWS Build\Windows\x86\Release\Object\WarlockEngine.res Build\Windows\x86\Release\Object\WarlockEngine.obj /OUT:Build\Windows\x86\Release\WarlockEngine.dll
-link /nologo /DEBUG:FULL /DLL /SUBSYSTEM:WINDOWS Build\Windows\x86\Debug\Object\WarlockEngine.res Build\Windows\x86\Debug\Object\WarlockEngine.obj /OUT:Build\Windows\x86\Debug\WarlockEngine.dll
+link /nologo /DLL /SUBSYSTEM:WINDOWS /VERBOSE Build\Windows\x86\Release\Object\WarlockEngine.res Build\Windows\x86\Release\Object\WarlockEngine.obj /OUT:Build\Windows\x86\Release\WarlockEngine.dll > Build\Windows\x86\Release\Log\Linker.log
+link /nologo /DEBUG:FULL /DLL /SUBSYSTEM:WINDOWS /VERBOSE Build\Windows\x86\Debug\Object\WarlockEngine.res Build\Windows\x86\Debug\Object\WarlockEngine.obj /OUT:Build\Windows\x86\Debug\WarlockEngine.dll > Build\Windows\x86\Debug\Log\Linker.log
